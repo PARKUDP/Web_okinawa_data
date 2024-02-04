ae9a57a3f1bb43252e7f34281b5ae0b274b9e924
e225752 <e225752@ie.u-ryukyu.ac.jp>
2024-02-04 13:52:55 +0900

    add

--- pages/file_update.py
+++ pages/file_update.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import urllib.request as urllib2  
 import json
+import os
+
+current_dir = os.path.dirname(__file__)
+data_key_path = os.path.join(current_dir, 'data','key.csv')
 
 # 医療関連のデータのキーを取得する関数
 def medical_key():
-    read_file = pd.read_csv("data/key.csv")
+    read_file = pd.read_csv(data_key_path)
     medical_data = read_file['医療']
     medical_data.fillna("None", inplace=True)
     medical_data_list = medical_data.tolist()
@@ -13,7 +17,7 @@ def medical_key():
 
 # 避難場所のデータのキーを取得する関数
 def hinann_key():
-    read_file = pd.read_csv("data/key.csv")
+    read_file = pd.read_csv(data_key_path)
     hinann_data = read_file['避難']
     hinann_data.fillna("None", inplace=True)
     hinann_data_list = hinann_data.tolist()
@@ -22,7 +26,7 @@ def hinann_key():
 
 # 文化のデータのキーを取得する関数
 def culture_key():
-    read_file = pd.read_csv("data/key.csv")
+    read_file = pd.read_csv(data_key_path)
     culture_data = read_file['文化']
     culture_data.fillna("None", inplace=True)
     culture_data_list = culture_data.tolist()
@@ -32,7 +36,7 @@ def culture_key():
 
 # 避難場所のデータのキーを取得する関数
 def Aed_key():
-    read_file = pd.read_csv("data/key.csv")
+    read_file = pd.read_csv(data_key_path)
     Aed_data = read_file['AED']
     Aed_data.fillna("None", inplace=True)
     Aed_data_list = Aed_data.tolist()
@@ -51,20 +55,24 @@ def get_data(resources, filename):
 
 def main(zyouki):
     if(zyouki == "medicalcare"):
+        medical_path = os.path.join(current_dir, 'data','medicalcare.csv')
         resources = medical_key()
         data = get_data(resources, "medicalcare")
-        data.to_csv("data/medicalcare.csv", index=False)
+        data.to_csv(medical_path, index=False)
     elif(zyouki == "hinann"):
+        hinanan_path = os.path.join(current_dir, 'data','hinann.csv')
         resources = hinann_key()
         data = get_data(resources, "hinann")
-        data.to_csv("data/hinann.csv", index=False)
+        data.to_csv(hinanan_path, index=False)
 
     elif(zyouki == "culture"):
+        culture_path = os.path.join(current_dir, 'data','culture.csv')
         resources = culture_key()
         data = get_data(resources, "culture")
-        data.to_csv("data/culture.csv", index=False)
+        data.to_csv(culture_path, index=False)
 
     elif(zyouki == "Aed"):
+        Aed_path = os.path.join(current_dir, 'data','Aed.csv')
         resources = Aed_key()
         data = get_data(resources, "Aed")
-        data.to_csv("data/Aed.csv", index=False)
+        data.to_csv(Aed_path, index=False)
